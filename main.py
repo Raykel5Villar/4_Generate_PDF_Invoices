7cec6bacc7097c6906ae085ce463c173371b065b
Raykel5Villar <raykelvillar@gmail.com>
2024-05-05 21:56:00 -0400

    add header, logo, and company name

--- main.py
+++ main.py
@@ -38,14 +38,29 @@ for filepath in filepaths:
     for index, row in df.iterrows():
         pdf.set_font(family="Times", size=10)
         pdf.set_text_color(80, 80, 80)
-
         pdf.cell(w=30, h=8, txt=str(row['product_id']), border=1)
         pdf.cell(w=60, h=8, txt=str(row['product_name']), border=1)
         pdf.cell(w=40, h=8, txt=str(row['amount_purchased']), border=1)
         pdf.cell(w=30, h=8, txt=str(row['price_per_unit']), border=1)
         pdf.cell(w=30, h=8, txt=str(row['total_price']), border=1, ln=1)
 
+    total_sum = df['total_price'].sum()
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=60, h=8, txt="", border=1)
+    pdf.cell(w=40, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
 
+    # Add total sum
+    pdf.set_font(family="Times", size=10)
+    pdf.cell(w=30, h=8, txt=f"The total price is {total_sum}",ln=1)
+
+    # Add company name and logo
+    pdf.set_font(family="Times", size=10)
+    pdf.cell(w=30, h=8, txt=f"Raykel Villar S.A")
+    pdf.image("hired.png", w=10)
 
     pdf.output(f"PDFs/{filename}.pdf")
 
